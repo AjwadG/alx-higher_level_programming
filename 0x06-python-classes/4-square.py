585671ad97393f166a6f1d8cccc4e564642bf350
AjwadG <ajwadelgandouz@yahoo.com>
2023-10-24 14:23:48 +0200

    4. Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module of Square"""
+
+
+class Square:
+    """Represents a Square, with a name."""
+    def __init__(self, size=0):
+        """initilationg size value of Square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """area method return are of square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """gets the value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the value of size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
