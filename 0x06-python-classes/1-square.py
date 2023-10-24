a340d351d3484410bb7b1281b4b37c455b100963
AjwadG <ajwadelgandouz@yahoo.com>
2023-10-24 14:11:14 +0200

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module of Square"""
+
+
+class Square:
+    """Represents a Square, with a name."""
+    def __init__(self, size=0):
+        """initilationg size value of Square"""
+        self.__size = size
