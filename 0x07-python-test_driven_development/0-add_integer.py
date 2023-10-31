686249d5a68ddec13a37942d5616fe267553f442
AjwadG <ajwadelgandouz@yahoo.com>
2023-10-31 22:05:39 +0200

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,51 +1,27 @@
 #!/usr/bin/python3
-"""Module of Square"""
+"""add_integer module"""
 
 
-class Square:
-    """Represents a Square, with a name."""
-    def __init__(self, size=0, position=(0, 0)):
-        """initilationg size value of Square"""
-        self.size = size
-        self.position = position
+def add_integer(a, b=98):
+    """Adds two integers
+     Args:
+        a: number 1.
+        b: number 2 defaults to 98.
 
-    def area(self):
-        """area method return are of square"""
-        return self.__size ** 2
+    Raises:
+        TypeError: if a or b are not numbers
 
-    @property
-    def size(self):
-        """gets the value of size"""
-        return self.__size
+    Returns:
+        The sum of a and b.
+    """
 
-    @size.setter
-    def size(self, value):
-        """sets the value of size"""
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
 
-    @property
-    def position(self):
-        """gets the value of size"""
-        return self.__position
 
-    @position.setter
-    def position(self, value):
-        """sets the value of position"""
-        if (not isinstance(value, tuple) or len(value) != 2 or type(value[0])
-                != int or type(value[1]) != int or value[1] < 0 or
-                value[0] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
-    def my_print(self):
-        """prints a square of #"""
-        if self.__size == 0:
-            print()
-            return
-        print(self.__position[1] * '\n', end='', sep='')
-        for i in range(self.__size):
-            print(self.__position[0] * ' ', self.__size * '#', sep='')
+if __name__ == "__main__":
+    from doctest import testfile
+    testfile("tests/0-add_integer.txt")
