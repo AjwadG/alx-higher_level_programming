425f7ef2e43ec7946b02d3fb4b344fa27fea81d1
AjwadG <ajwadelgandouz@yahoo.com>
2023-10-31 22:58:55 +0200

    4. Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""text_indentation module"""
+
+
+def text_indentation(text):
+    """adding 2 newlines after ., ? and :.
+
+    Args:
+        text: string
+
+    Raises:
+        TypeError: text is not string.
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    for a in ".?:":
+        # print(delim, text.split(delim))
+        text = (a + "\n\n").join([line.strip(" ") for line in text.split(a)])
+
+    print(text, end="")
+
+
+if __name__ == "__main__":
+    from doctest import testfile
+    testfile("tests/5-text_indentation.txt")
