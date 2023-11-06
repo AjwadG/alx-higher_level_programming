1359b629e75747bea0a06d2b998c3883832136ac
AjwadG <ajwadelgandouz@yahoo.com>
2023-11-06 14:43:26 +0200

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry class module"""
+
+
+class BaseGeometry:
+    """BaseGeometry only to couse trouple"""
+    def area(self):
+        """rises error"""
+        raise Exception("area() is not implemented")
