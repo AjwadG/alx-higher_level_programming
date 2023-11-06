e18478f3c6ed3c751103f8008a1aabf44cfb6707
AjwadG <ajwadelgandouz@yahoo.com>
2023-11-06 14:35:05 +0200

    3. Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""is_kind_of_class function module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """obj is same or inhirated from oject"""
+    return isinstance(obj, a_class)
