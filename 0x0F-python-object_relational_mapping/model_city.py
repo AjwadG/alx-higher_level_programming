d54025da8762573f9db5038c287ddbb04c6d23a5
AjwadG <ajwadelgandouz@yahoo.com>
2024-01-13 14:48:33 +0200

    14. Cities in state

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" City Class def """
+from sqlalchemy import Column, Integer, String
+from model_state import Base
+from sqlalchemy import ForeignKey
+
+
+class City(Base):
+    """
+    City class sup calss from Base
+    ORM first try :D
+
+    Attributes:
+        id: id that will be asigned from the sql
+        name: name of the state
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
