2c09166469f571512759474191b2e4af681727a0
AjwadG <ajwadelgandouz@yahoo.com>
2024-01-26 23:15:49 +0200

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" fetches https://alx-intranet.hbtn.io/status """
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as req:
+        res = req.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode("UTF-8")))
