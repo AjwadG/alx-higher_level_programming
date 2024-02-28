16338d1e34ec73b9cf605be362804de4e8ac5cbe
AjwadG <ajwadelgandouz@yahoo.com>
2024-02-28 22:29:12 +0200

    10. No jQuery - document loaded

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,4 +1,6 @@
 const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
-$.ajax({ url }).done(function (data) {
-  $('DIV#hello').text(data.hello);
+$(document).ready(function () {
+  $.ajax({ url }).done(function (data) {
+    $('DIV#hello').text(data.hello);
+  });
 });
